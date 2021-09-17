72bc05517aa0c35784c906df89a9bf1795388a0b
Max HH <maxbowen1234@gmail.com>
2021-09-17 19:18:39 +0300

    Second edit and new Categories

--- src/main/java/com/example/bookstore1/Bookstore1Application.java
+++ src/main/java/com/example/bookstore1/Bookstore1Application.java
@@ -1,13 +1,50 @@
 package com.example.bookstore1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.bookstore1.domain.Book;
+import com.example.bookstore1.domain.BookRepository;
+import com.example.bookstore1.domain.Category;
+import com.example.bookstore1.domain.CategoryRepository;
+
+
+
+
 
 @SpringBootApplication
 public class Bookstore1Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Bookstore1Application.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Bookstore1Application.class, args);
 	}
 
+	
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
+		return (args) -> {
+				
+			log.info("save a couple of books");
+			crepository.save(new Category("Fiction"));
+			crepository.save(new Category("History"));
+			crepository.save(new Category("Fantasy"));
+			crepository.save(new Category("Horror"));
+		
+
+			
+			brepository.save(new Book("Sun Tzu", "Art of War", 1, "978010-22", crepository.findByName("History").get(0)));
+	
+			
+			log.info("fetch all books");
+			for (Book book : brepository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+	}
 }
--- src/main/java/com/example/bookstore1/web/BookController.java
+++ src/main/java/com/example/bookstore1/web/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.bookstore1.domain.Book;
 import com.example.bookstore1.domain.BookRepository;
+import com.example.bookstore1.domain.CategoryRepository;
 
 @Controller
 public class BookController {
@@ -16,34 +17,41 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
+	@Autowired
+	private CategoryRepository crepository;
+	
+	// Show all books
 	@RequestMapping(value = {"/", "/booklist"})
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
-	
+	// Add new book
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
+		model.addAttribute("categorys", crepository.findAll());
 		return "addBook";
 	}
-	
+	// Save new book
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
 	}
 	
+	// Delete a book
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
 		
 	}
-	
+	// Edit a book
 	@RequestMapping(value ="/edit/{id}")
 	public String editBook(@PathVariable("id")Long bookId, Model model) {
 		model.addAttribute("book", repository.findById(bookId));
+		model.addAttribute("categorys", crepository.findAll());
 		return "editBook";
 	
 	}
