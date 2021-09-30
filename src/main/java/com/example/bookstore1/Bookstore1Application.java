e6524549b888922f12061613d3ffc478885da240
Max HH <maxbowen1234@gmail.com>
2021-09-30 17:43:52 +0300

    Adding login page and logout

--- src/main/java/com/example/bookstore1/Bookstore1Application.java
+++ src/main/java/com/example/bookstore1/Bookstore1Application.java
@@ -11,6 +11,8 @@ import com.example.bookstore1.domain.Book;
 import com.example.bookstore1.domain.BookRepository;
 import com.example.bookstore1.domain.Category;
 import com.example.bookstore1.domain.CategoryRepository;
+import com.example.bookstore1.domain.User;
+import com.example.bookstore1.domain.UserRepository;
 
 
 
@@ -27,7 +29,7 @@ public class Bookstore1Application {
 
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 				
 			log.info("save a couple of books");
@@ -40,6 +42,11 @@ public class Bookstore1Application {
 			
 			brepository.save(new Book("Sun Tzu", "Art of War", 1, "978010-22", crepository.findByName("History").get(0)));
 	
+			// Creating users: admin/admin user/user
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 			
 			log.info("fetch all books");
 			for (Book book : brepository.findAll()) {
