bb6cb82a9daaf18ddc04ffda94dce24c18c69523
Max HH <maxbowen1234@gmail.com>
2021-10-14 11:12:28 +0300

    changes to test

--- src/test/java/com/example/bookstore1/BookRepositoryTest.java
+++ src/test/java/com/example/bookstore1/BookRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.example.bookstore1.domain.Book;
@@ -14,7 +15,7 @@ import com.example.bookstore1.domain.BookRepository;
 import com.example.bookstore1.domain.Category;
 
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
+@SpringBootTest
 public class BookRepositoryTest {
 
 	
